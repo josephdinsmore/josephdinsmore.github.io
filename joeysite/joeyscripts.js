24526f329073349d7ced6b9a728c0aa966a20ccd
josephdinsmore <josephdinsmore@berkeley.edu>
2020-07-02 21:19:51 -0400

    initial commit

---
+++ joeysite/joeyscripts.js
@@ -0,0 +1,24 @@
+//dark mode
+function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode"); }
+
+//
+
+var texts = [
+    "He knew at last what he wanted, but in finding it out it seemed that he had put it forever beyond his grasp.",
+    "If you don't understand it without an explanation, you can't understand it with an explanation.",
+    "Spiritual love from the waist up and physical love from the waist down.",
+    "They slipped briskly into an intimacy from which they never recovered.",
+    "Love is a stove that burns too much.",
+    "There is no greater glory than to die for love.",
+    "But if they had learned anything together, it was that wisdom comes to us when it can no longer do any good."
+  ];
+  
+  document.getElementById('randomText').value = texts[Math.floor(Math.random()*texts.length)];
+
+//
+
+//
+
+//
